c737b50f152531870967826e73d2da7ec3983333
ItsEvdok <semionaevdokimov@icloud.com>
2021-05-16 21:35:06 -0400

    finished console printing Departments, Roles, and Employees

--- app.js
+++ app.js
@@ -40,12 +40,30 @@ const employeeData = () => {
 
 const viewDepartments = () => {
   const sql = `SELECT * FROM department`;
+
+  db.query(sql, (err, row) => {
+    if (err) {
+      console.log("No departments found.");
+      return;
+    }
+    console.table(row);
+    employeeData();
+  });
 };
 
 const viewRoles = () => {
   const sql = `SELECT role.id, role.title, department.department_name, role.salary
                 FROM role
                 LEFT JOIN department ON role.department_id = department.id;`;
+
+  db.query(sql, (err, row) => {
+    if (err) {
+      console.log("No departments found.");
+      return;
+    }
+    console.table(row);
+    employeeData();
+  });
 };
 
 const viewEmployees = () => {
@@ -53,74 +71,74 @@ const viewEmployees = () => {
                 FROM employee
                 LEFT JOIN role ON employee.role_id = role.id
                 LEFT JOIN department ON role.department_id = department.id;`;
+  db.query(sql, (err, row) => {
+    if (err) {
+      console.log("No departments found.");
+      return;
+    }
+    console.table(row);
+    employeeData();
+  });
 };
 
 const addDepartment = () => {
-    return inquirer 
-        .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the new departments name?'
-        }
-    ])
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the new departments name?",
+    },
+  ]);
 };
 
 const addRole = () => {
-    return inquirer 
-        .prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is the new roles name?'
-        },
-        {
-            type: 'number',
-            name: 'salary',
-            message: 'What is the new roles salary?'
-        },
-        {
-            type: 'list',
-            name: 'department',
-            message: 'What department does the new role belong to?',
-            choices: [
-                "Sales",
-                "Engineering",
-                "Finance",
-                "Legal"
-            ]
-        }
-    ])
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the new roles name?",
+    },
+    {
+      type: "number",
+      name: "salary",
+      message: "What is the new roles salary?",
+    },
+    {
+      type: "list",
+      name: "department",
+      message: "What department does the new role belong to?",
+      choices: ["Sales", "Engineering", "Finance", "Legal"],
+    },
+  ]);
 };
 
 const addEmployee = () => {
-    return inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'first',
-                message: 'What is the new employees first name?'
-            },
-            {
-                type: 'input',
-                name: 'last',
-                message: 'What is the new employees last name?'
-            },
-            {
-                type: 'list',
-                name: 'role',
-                message: 'What is the new employees role?',
-                choices: [
-                    "Sales Lead",
-                    "Salesperson",
-                    "Lead Engineer",
-                    "Software Engineer",
-                    "Accountant",
-                    "Legal Team Lead",
-                    "Lawyer"
-                ]
-            },
-        ])
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "first",
+      message: "What is the new employees first name?",
+    },
+    {
+      type: "input",
+      name: "last",
+      message: "What is the new employees last name?",
+    },
+    {
+      type: "list",
+      name: "role",
+      message: "What is the new employees role?",
+      choices: [
+        "Sales Lead",
+        "Salesperson",
+        "Lead Engineer",
+        "Software Engineer",
+        "Accountant",
+        "Legal Team Lead",
+        "Lawyer",
+      ],
+    },
+  ]);
 };
 
 employeeData();

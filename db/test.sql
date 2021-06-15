c5d5120487add6e41c5b3a047095898e688ef92c
ItsEvdok <semionaevdokimov@icloud.com>
2021-06-14 21:23:36 -0400

    Update test.sql

--- db/test.sql
+++ db/test.sql
@@ -1,4 +1,3 @@
-SELECT employee.id,  employee.first_name, employee.last_name, role.title, role.salary, department.department_name
-    FROM employee
-    LEFT JOIN role ON employee.role_id = role.id
+SELECT role.id, role.title, department.department_name, role.salary
+    FROM role
     LEFT JOIN department ON role.department_id = department.id;
